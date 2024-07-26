f459dff7c896c47a55ec8909675508c409e51743
Javier Aldeguer Copete <javier.aldeguer1@gmail.com>
2024-07-27 01:40:07 +0200

    Comentario

--- src/pruebaBucles2.java
+++ src/pruebaBucles2.java
@@ -3,29 +3,29 @@ import javax.swing.JOptionPane;
 public class pruebaBucles2 {
 
 	public static void main(String[] args) {
-		
-		int randomNum = (int)(100*Math.random()); //refundicion de doble a entero
+
+		int randomNum = (int) (100 * Math.random()); // refundicion de doble a entero
 		int intentos = 0;
-		
+
 		int userNum = Integer.parseInt(JOptionPane.showInputDialog("Adivina el número del 0 al 100."));
-		
-		while (userNum!=randomNum) {
+
+		while (userNum != randomNum) {
 			intentos++;
-			if (userNum<randomNum) {
+			if (userNum < randomNum) {
 				userNum = Integer.parseInt(JOptionPane.showInputDialog("Incorrecto, el número es mayor."));
-			}
-			else {
+			} else {
 				userNum = Integer.parseInt(JOptionPane.showInputDialog("Incorrecto, el número es menor."));
 			}
 		}
-		
-		if (intentos<=7) {
+
+		// esta función revisa si se ha llegado al número de intentos máximo
+		if (intentos <= 7) {
 			JOptionPane.showMessageDialog(null, "¡Correcto! Lo has intentado " + intentos + " veces, bien hecho.");
+		} else {
+			JOptionPane.showMessageDialog(null,
+					"¡Correcto! Lo has intentado " + intentos + " veces, a la próxima mejor.");
 		}
-		else {
-			JOptionPane.showMessageDialog(null, "¡Correcto! Lo has intentado " + intentos + " veces, a la próxima mejor.");
-		}
-		
+
 	}
 
 }
